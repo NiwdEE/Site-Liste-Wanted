fc7dfdeae2fc2a10647a0b983f0e28c5c7c62474
Edwin AMREIN <ed.alb180@gmail.com>
2022-02-05 14:16:14 +0100

    Début carrousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { CardPreviewDirective } from './misc/card-preview.directive';
 import { FooterComponent } from './footer/footer.component';
 import { VoyagesComponent } from './voyages/voyages.component';
 
+import 'hammerjs';
+import { NgxHmCarouselModule } from 'ngx-hm-carousel';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +36,8 @@ import { VoyagesComponent } from './voyages/voyages.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxHmCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/voyages/voyages.component.ts
+++ src/app/voyages/voyages.component.ts
@@ -7,14 +7,29 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 })
 export class VoyagesComponent implements OnInit {
 
-  hidden: boolean;
+  currentIndex = 0;
+  speed = 5000;
+  infinite = true;
+  direction = 'right';
+  directionToggle = true;
+  autoplay = true;
+  avatars = '1234567891234'.split('').map((x, i) => {
+    const num = i;
+    // const num = Math.floor(Math.random() * 1000);
+    return {
+      url: `https://picsum.photos/600/400/?${num}`,
+      title: `${num}`,
+    };
+  });
 
-  constructor(){
-    this.hidden = true;
+  constructor() {}
+
+  click(i: any) {
+    alert(`${i}`);
   }
 
   ngOnInit(): void {
-    this.hidden = false;
+    
   }
 
 
