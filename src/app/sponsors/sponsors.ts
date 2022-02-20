a0dcbf00ea54851d379c30fa3605a47acf7a8a5d
NiwdEE <ed.alb180@gmail.com>
2022-02-20 15:37:04 +0100

    Sponsors => Logos (ntm adblock)

--- src/app/sponsors/sponsors.ts
+++ src/app/sponsors/sponsors.ts
@@ -13,19 +13,19 @@ export const sponsors: sponsor[] = [
     {
         name: "Brief.me ",
         desc: 'Vous partage l’essentiel de l’actualité de manière quotidienne via l’application ou par mail. Grâce à Wanted, les 2 mois d’essais sont gratuits et l’abonnement annuel revient à 29€ au lieu de 58,80€.',
-        logo: 'brief.me.png',
+        logo: 'BM.png',
         link: "https://www.brief.me/"
     },
     {
         name: "Tripcounter",
         desc: "Vous propose de l’aide pour trouver stage, logement et assistance à l’étranger. Grâce à Wanted, une réduction de 5 % est applicable sur chacun des services proposés et il y aura possibilité de gagner des services grâce à des concours",
-        logo: 'TripCounter.png',
+        logo: 'TC.png',
         link: "https://tripcounter.fr/"
     },
     {
         name: "Pumpkin",
         desc: "Vous propose des réductions grâce au code parrainage Wanted, et nous permettera d'avoir accès à une billeterie simple et éfficace",
-        logo: 'pumpkin.png',
+        logo: 'PK.png',
         link: "https://pumpkin-app.co"
     },
     {
@@ -37,24 +37,24 @@ export const sponsors: sponsor[] = [
     {
         name: "Pop out",
         desc: "Une super application de bons plans sur Paris et ses alentours.",
-        logo: 'pop_out.png'
+        logo: 'PO.png'
     },
     {
         name: "Poppy’s",
         desc: "Une excellente crèperie qui vous proposera un délicieux menu Wanted.",
-        logo: 'Poppy_s.png',
+        logo: 'Pops.png',
         link: "https://www.poppy-s.fr"
     },
     {
         name: "Koezio",
         desc: "Des réductions sur un tas d'activitées telles que des escape games ou de la réalité virtuelle",
-        logo: 'Koezio.png',
+        logo: 'Kz.png',
         link: 'https://koezio.co'
     },
     {
         name: "Permilike",
         desc: "Une super auto-école située dans Cergy qui vous propose une scéance d'évaluation gratuite (d'une valeur de 44€).",
-        logo: 'PermiLike.png',
+        logo: 'PL.png',
         link: "https://www.permilike.com"
     },
     {
@@ -66,6 +66,6 @@ export const sponsors: sponsor[] = [
     {
         name: "BeEtudiant",
         desc: 'Une super application de bons plans sur Cergy et ses alentours.',
-        logo: 'beEtudiant.png'
+        logo: 'BEE.png'
     },
 ]
